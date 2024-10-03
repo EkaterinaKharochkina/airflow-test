bac5424e39805ee29596e7cff268506cde4011fa
ekaterinaharockina <@>
2024-10-03 17:16:13 +0300

    Clone repository

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -2,10 +2,17 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
+# function definition
+
+def foo(name):
+    print(f'Hello, {name}!')
+
+# DAG logic
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 10, 2),
+    'start_date': datetime(2024, 10, 3),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -18,7 +25,12 @@ dag = DAG('my_hello_world',
           catchup=False
 )
 
-def foo():
-    print('Hello World!')
-
-print_hello_world = PythonOperator(task_id='print_hello_world', dag=dag, python_callable=foo)
+print_hello_world = PythonOperator(task_id='print_hello_world',
+                                   dag=dag,
+                                   python_callable=foo,
+                                   op_kwargs = {'name':'Katya'})
+print_hello_world_2 = PythonOperator(task_id='print_hello_world_2',
+                                   dag=dag,
+                                   python_callable=foo,
+                                   op_args = ['Katya'])
+print_hello_world>>print_hello_world_2
